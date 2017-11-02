6e916771107c78368c647b21bb8a8883c86f3e16
Fabien Lanoux <fabien.lanoux@tms-misiv.fr>
2017-11-02 14:54:33 +0100

    ajouté support pour détecter autoload.php si exemples run en root project ou dépendence

--- examples/init.php
+++ examples/init.php
@@ -1,6 +1,11 @@
 <?php
 
-require '../vendor/autoload.php';
+if (file_exists($a = __DIR__ . '/../../../autoload.php')) {
+    require_once $a;
+} else {
+    require_once __DIR__ . '/../vendor/autoload.php';
+}
+
 
 use Dkd\PhpCmis\SessionFactory;
 use Dkd\PhpCmis\SessionParameter;
